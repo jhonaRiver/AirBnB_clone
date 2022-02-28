023a1d15b9b759eab6a4698797b641b829dfdcb1
Drew-24 <andrewwil1@hotmail.com>
2022-02-28 14:33:21 -0400

    First try to fully build our base

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,26 @@
 ...
 """
 from datetime import datetime
+from uuid import uuid4
+
 
 class BaseModel:
     """
     ...
     """
 
+    def __init__(self):
+        ''''''
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        ''''''
+        return ('[BaseModel] ({}) {}'.format(self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
