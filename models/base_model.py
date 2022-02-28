e6d0c5fb948aac8be1fed14fe932a796e2c98a6d
Drew-24 <andrewwil1@hotmail.com>
2022-02-28 15:35:54 -0400

    Finished 3rd task with first changes to our class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-...
+Module holds BaseModel class
 """
 from datetime import datetime
-from uuid import uuid4
+import uuid
+from xmlrpc.client import _iso8601_format
+import json
 
 
 class BaseModel:
     """
-    ...
+    class that holds the base of our Airbnb clone console
     """
 
     def __init__(self):
-        ''''''
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        ''' class initializer '''
+        self.id = str(uuid.uuid4())
+        self.created_at = _iso8601_format(datetime.now())
+        self.updated_at = _iso8601_format(datetime.now())
 
     def __str__(self):
-        ''''''
+        ''' string representation of a class '''
         return ('[BaseModel] ({}) {}'.format(self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime.now()
+        ''' saves an object to JSON file '''
+        self.updated_at = _iso8601_format(datetime.now())
 
     def to_dict(self):
-        return self.__dict__
+        ''' returns dictionary containing key value of dictionary '''
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
