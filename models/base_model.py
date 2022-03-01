fbe9b190b376006840bed86cb864d73962ff39f4
Drew-24 <andrewwil1@hotmail.com>
2022-03-01 14:42:00 -0400

    Fixed isoformat and number format with the help of solymar and Luis

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,19 @@ class BaseModel:
     class that holds the base of our Airbnb clone console
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' class initializer '''
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-        self.created_at = _iso8601_format(datetime.now())
-        self.updated_at = _iso8601_format(datetime.now())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                self.__dict__[key] = value
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, time_format)
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         ''' string representation of a class '''
@@ -25,10 +33,12 @@ class BaseModel:
 
     def save(self):
         ''' saves an object to JSON file '''
-        self.updated_at = _iso8601_format(datetime.now())
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         ''' returns dictionary containing key value of dictionary '''
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
