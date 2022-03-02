c45cace71d96c4e36c8bff46c9a22fe24ce2cd0e
Andrew González <85302191+Drew-24@users.noreply.github.com>
2022-03-02 16:23:45 +0000

    Added more instances

--- console.py
+++ console.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python3
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
 
-    prompt = "(hbnb) "
+	prompt = "(hbnb) "
+	allowed_classes = ['BaseModel']
+	
+	def do_quit(self, arg):
+		'''Quit command to exit the program'''
+		return True
 
-    def do_quit(self, arg):
-        return True
+	def do_EOF(self, arg):
+		''' End of File '''
+		print("")
+		return True
 
-    def do_EOF(self, arg):
-        print("")
-        return True
+	def help(self):
+		''' help command '''
+		return True
 
-    def help(self):
-        return True
+	def emptyline(self):
+		''' registers an empty line and does a pass '''
+		pass
 
-    def EmptyLine(self):
-        pass
+	def do_create(self, line):
+		''' creates a new class instance of BaseModel'''
+		command = self.parseline(line)[0]
+		if command is None:
+			print('** class name is missing **')
+		elif command not in self.allowed_classes:
+			print("** class doesn't exist **")
+		else:
+			new_obj = eval(command)()
+			print(new_obj.id)
 
+	def do_show(self, line):
+		'''Prints the string representation of an instance based on the class name and id'''
+		command = self.parseline(line)[0]
+		arg = self.parseline(line)[1]
+		if command is None:
+			print('** class name is missing **')
+		elif command not int self.allowed_classes:
+			print("** class doesn't exist **")
+		elif arg == '':
+			print('** instance id missing **')
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+	HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -5,40 +5,43 @@ Module holds BaseModel class
 from datetime import datetime
 import uuid
 from xmlrpc.client import _iso8601_format
-import json
+import models
 
 
 class BaseModel:
-    """
-    class that holds the base of our Airbnb clone console
-    """
+	"""
+	class that holds the base of our Airbnb clone console
+	"""
 
-    def __init__(self, *args, **kwargs):
-        ''' class initializer '''
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                self.__dict__[key] = value
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, time_format)
-                if key != "__class__":
-                    setattr(self, key, value)
+	def __init__(self, *args, **kwargs):
+		''' class initializer '''
+		time_format = "%Y-%m-%dT%H:%M:%S.%f"
+		self.id = str(uuid.uuid4())
+		self.created_at = datetime.utcnow()
+		self.updated_at = datetime.utcnow()
+		if len(kwargs) != 0:
+			for key, value in kwargs.items():
+				self.__dict__[key] = value
+				if key == 'created_at' or key == 'updated_at':
+					value = datetime.strptime(value, time_format)
+					if key != "__class__":
+						setattr(self, key, value)
+		else:
+			models.storage.new(self)
 
-    def __str__(self):
-        ''' string representation of a class '''
-        return ('[BaseModel] ({}) {}'.format(self.id, self.__dict__))
+def __str__(self):
+	''' string representation of a class '''
+	return ('[BaseModel] ({}) {}'.format(self.id, self.__dict__))
 
-    def save(self):
-        ''' saves an object to JSON file '''
-        self.updated_at = datetime.utcnow()
+def save(self):
+	''' saves an object to JSON file '''
+	self.updated_at = datetime.utcnow()
+	models.storage.save()
 
-    def to_dict(self):
-        ''' returns dictionary containing key value of dictionary '''
-        dictionary = self.__dict__.copy()
-        dictionary["__class__"] = self.__class__.__name__
-        dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+def to_dict(self):
+	''' returns dictionary containing key value of dictionary '''
+	dictionary = self.__dict__.copy()
+	dictionary["__class__"] = self.__class__.__name__
+	dictionary["created_at"] = self.created_at.isoformat()
+	dictionary["updated_at"] = self.updated_at.isoformat()
+	return dictionary
