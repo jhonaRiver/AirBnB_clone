286a0035c2a682b4d954a31fec8b4a5d39c2986f
Drew-24 <andrewwil1@hotmail.com>
2022-03-03 13:19:27 -0400

    Added documentation and imported User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+""" Module holds FileStorage class """
 from models.base_model import BaseModel
 import json
+from models.user import User
 
 
 class FileStorage:
+    """ Storage Engine
+
+    Args:
+        __file_path (str): Name of file were objects are stored
+        __objects (dict): A dictionary of objects
+    """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """ Retunrs Dict objects """
         return FileStorage.__objects
 
     def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
         object_class_name = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(object_class_name, obj.id)] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file (path: __file_path) """
         object_dictionary_save = {}
         with open(self.__file_path, 'w') as f:
             for key, value in self.__objects.items():
@@ -22,6 +34,7 @@ class FileStorage:
             json.dump(object_dictionary_save, f)
 
     def reload(self):
+        """ deserializes the JSON file to __objects, if it exists """
         try:
             with open(FileStorage.__file_path) as f:
                 object_dictionary = json.load(f)
