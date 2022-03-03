1ba6960622c5691476e70018d444c7463a186889
Drew-24 <andrewwil1@hotmail.com>
2022-03-03 13:10:27 -0400

    Created User class

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module holds class that inherits from BaseModel """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ class that represents a User 
+    Args:
+        email (str): User email
+        password (str): User Password
+        first_name (str): User name
+        last_name (str): User last name
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
