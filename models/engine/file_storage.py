da82363d5504ae6145b0bf1a75e228e17ea3778e
Drew-24 <andrewwil1@hotmail.com>
2022-03-03 14:00:44 -0400

    Added classes import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,11 @@
 from models.base_model import BaseModel
 import json
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
 
 
 class FileStorage:
