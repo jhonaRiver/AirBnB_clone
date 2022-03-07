f0e57d236b692c7c20a2b139a9b9941b07c3eb8c
Drew-24 <andrewwil1@hotmail.com>
2022-03-06 23:44:45 -0400

    Fixed code

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Creation of storage variable as an instance of FileStorage class """
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
