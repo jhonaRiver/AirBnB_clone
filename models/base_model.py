9a217b0fc83a3424ca6a9ee6fa0d1e19dee3c16b
Drew-24 <andrewwil1@hotmail.com>
2022-03-07 09:12:40 -0400

    Fixed comments

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,9 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        ''' class initializer '''
+        """
+        class initializer
+        """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
@@ -30,7 +32,9 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        ''' string representation of a class '''
+        """
+        string representation of a class
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
@@ -46,7 +50,9 @@ class BaseModel:
     """ Public instance method """
 
     def to_dict(self):
-        ''' returns dictionary containing key value of dictionary '''
+        """
+        returns dictionary containing key value of dictionary
+        """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = self.created_at.isoformat()
