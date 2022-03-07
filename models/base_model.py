9150cf63d711af5eb7d5095506a58d38acb02882
Drew-24 <andrewwil1@hotmail.com>
2022-03-07 09:16:14 -0400

    Fixed code

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,6 @@ class BaseModel:
         class initializer
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 self.__dict__[key] = value
@@ -29,6 +26,9 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
 
     def __str__(self):
